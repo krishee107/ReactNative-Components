0c3011c22ce3e610359a71c331971ce514af5be1
krishee107 <krishee107@gmail.com>
2023-04-29 22:02:02 +0200

    Alert con prompt

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -2,8 +2,25 @@ import React from 'react'
 import { Alert, Button, View } from 'react-native'
 import { HeaderTitle } from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
+import prompt from 'react-native-prompt-android'
 
 export const AlertScreen = () => {
+
+    prompt(
+        'Enter password',
+        'Enter your password to claim your $1.5B in lottery winnings',
+        [
+            { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+            { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+        ],
+        {
+            type: 'secure-text',
+            cancelable: false,
+            defaultValue: 'test',
+            placeholder: 'placeholder'
+        }
+    );
+
     return (
         <View
             style={
