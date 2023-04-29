fa77c2fbb49aa7374113d4bb2bfa66b8cf3ace5a
krishee107 <krishee107@gmail.com>
2023-04-29 22:27:49 +0200

    Solucion para ver los inputs sin que moleste el teclado y permitir hacer scroll

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, StyleSheet, TextInput, View } from 'react-native'
 import { HeaderTitle } from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
+import { ScrollView } from 'react-native-gesture-handler'
 
 export const TextInputScreen = () => {
     const [form, setForm] = useState({
@@ -18,38 +19,50 @@ export const TextInputScreen = () => {
     }
 
     return (
-        <View style={styles.globalMargin}>
-            <HeaderTitle title='TextInput' />
+        <KeyboardAvoidingView // Permite hacer scroll cuando el teclado aparece para poder ver los inputs
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'} // Padding para iOS, height para Android
+        >
+            {/*Permite hacer scroll cuando el teclado aparece para poder ver los inputs*/}
+            <ScrollView>
 
-            <TextInput
-                style={stylesScreen.inputStyle}
-                placeholder='Ingrese su nombre' // Placeholder text
-                autoCorrect={false} // Disable autocorrect
-                autoCapitalize='words' // Capitalize words
-                onChangeText={(value) => onChange(value, 'name')} // Update form.name
-            />
+                <View style={styles.globalMargin}>
+                    <HeaderTitle title='TextInput' />
 
-            <TextInput
-                style={stylesScreen.inputStyle}
-                placeholder='Ingrese su email'
-                autoCorrect={false}
-                autoCapitalize='none' // Do not capitalize
-                keyboardType='email-address' // Show email keyboard
-                onChangeText={(value) => onChange(value, 'email')} // Update form.email
-                keyboardAppearance='dark' // Dark keyboard appearance (iOS only)
-            />
+                    <TextInput
+                        style={stylesScreen.inputStyle}
+                        placeholder='Ingrese su nombre' // Placeholder text
+                        autoCorrect={false} // Disable autocorrect
+                        autoCapitalize='words' // Capitalize words
+                        onChangeText={(value) => onChange(value, 'name')} // Update form.name
+                    />
 
-            <TextInput
-                style={stylesScreen.inputStyle}
-                placeholder='Ingrese su teléfono'
-                autoCorrect={false}
-                keyboardType='phone-pad' // Show numeric keyboard
-                onChangeText={(value) => onChange(value, 'phone')} // Update form.phone
-            />
+                    <TextInput
+                        style={stylesScreen.inputStyle}
+                        placeholder='Ingrese su email'
+                        autoCorrect={false}
+                        autoCapitalize='none' // Do not capitalize
+                        keyboardType='email-address' // Show email keyboard
+                        onChangeText={(value) => onChange(value, 'email')} // Update form.email
+                        keyboardAppearance='dark' // Dark keyboard appearance (iOS only)
+                    />
 
 
-            <HeaderTitle title={JSON.stringify(form, null, 3)} />
-        </View>
+                    <HeaderTitle title={JSON.stringify(form, null, 3)} />
+                    <HeaderTitle title={JSON.stringify(form, null, 3)} />
+                    <TextInput
+                        style={stylesScreen.inputStyle}
+                        placeholder='Ingrese su teléfono'
+                        autoCorrect={false}
+                        keyboardType='phone-pad' // Show numeric keyboard
+                        onChangeText={(value) => onChange(value, 'phone')} // Update form.phone
+                    />
+
+
+                </View>
+            </ScrollView>
+
+        </KeyboardAvoidingView>
+
     )
 }
 const stylesScreen = StyleSheet.create({
